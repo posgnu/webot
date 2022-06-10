b132ec14b4158ebb1eb95543afa7c5b3981e4718
Geunwoo Kim <posgnu@gmail.com>
2022-06-09 23:19:40 -0700

    Update .gitignore

--- Chapter06/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Setup script to install chromium chromedriver for agent's browser interaction
-# Chapter 6, TensorFlow 2 Reinforcement Learning Cookbook | Praveen Palanisamy
-
-sudo apt-get install chromium-chromedriver
